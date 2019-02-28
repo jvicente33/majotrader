3b406d0f53045f223816af84e7911d77277766d9
J. Daza <blackencio33@gmail.com>
2019-02-28 13:58:36 -0400

    Primera subida

---
+++ src/components/BannerOne.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="banner-area relative" id="home">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+      <div class="row fullscreen d-flex align-items-center justify-content-center">
+        <div class="banner-content col-lg-12 col-md-12">
+          <h6 class="text-uppercase">Don’t look further, here is the key</h6>
+          <h1>We’re Industrial solution</h1>
+          <p
+            class="text-white"
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            <br>dolore magna aliqua. Ut enim ad minim. sed do eiusmod tempor incididunt.
+          </p>
+          <a href="#" class="primary-btn header-btn text-uppercase">Get Started</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
