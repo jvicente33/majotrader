47ba5d8cfd75a8837c70201aeeb706ccce7c685b
J. Daza <blackencio33@gmail.com>
2019-05-14 16:38:16 -0400

    modify nodemailer

--- src/server/nodemailer.js
+++ src/server/nodemailer.js
@@ -1,35 +1,104 @@
-"use strict";
-const nodemailer = require("nodemailer");
+'use strict';
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+const nodemailerSendgrid = require('nodemailer-sendgrid');
+const key = require('./key-tradersplanet.json')
+
+/**
+ * blog
+ * SG.-1KpRCh8QDGXShWanbncHQ.7sVsMRE46Xh-c6WbC-omCblV2SDRblchcPLHKHaswJg
+ * 
+ * tradersplanet-blog
+ * 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
+ */
+
+/*
+idClient: 481274889427-852dmbk1rbr32rktim7kcv65r2nri00t.apps.googleusercontent.com
+keySecret: lmV9O0iBV0VsCE4v_BLUUJdo
+*/
 
 async function sendMail(subject, text, name, email) {
   try {
     // create reusable transporter object using the default SMTP transport
+    /*const oauth2Client = new OAuth2(
+      '481274889427-852dmbk1rbr32rktim7kcv65r2nri00t.apps.googleusercontent.com',
+      'lmV9O0iBV0VsCE4v_BLUUJdo',
+      'https://developers.google.com/oauthplayground'
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token:
+        '1/IwQviU9lUMaKxsDhDkZbSZezw1xciUClIiJt-RMjYsT8UazsjOCXqk-bW0ezxoqa'
+    });
+    const tokens = await oauth2Client.refreshAccessToken();
+    const accessToken = tokens.credentials.access_token;*/
+
+    /*const transport = nodemailer.createTransport(
+      nodemailerSendgrid({
+          apiKey: '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'
+      })
+  );*/
+
     let transporter = nodemailer.createTransport({
-      host: "smtp-relay.gmail.com",
-      port: 587,
-      secure: true, // true for 465, false for other ports
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, 
       auth: {
-        user: "admin@tradersplanet.us",
-        pass: "administrador"
-      },
+        type: 'OAuth2',
+        user: 'admin@tradersplanet.us',
+        serviceClient: key.client_id,
+        privateKey: key.private_key
+      }
+      
+      /*,
       tls: {
         rejectUnauthorized: false
-      }
+      }*/
+      /*host: 'smtp.sendgrid.net',
+      port: 465,
+      secure: true,
+      auth: {
+        type: 'OAuth2',
+        user: 'admin@tradersplanet.us',
+        clientId:
+          '481274889427-852dmbk1rbr32rktim7kcv65r2nri00t.apps.googleusercontent.com',
+        clientSecret: 'lmV9O0iBV0VsCE4v_BLUUJdo',
+        refreshToken:
+          '1/IwQviU9lUMaKxsDhDkZbSZezw1xciUClIiJt-RMjYsT8UazsjOCXqk-bW0ezxoqa',
+        accessToken:
+          'ya29.GlsJB2N8VlCb4EdJKUSuFLArAJobUi3bliOonCalSmW00ZjslEhrkyXL6XFNghXEglRrg_8U8mAXrDkKQYpa2v09SehbujebKd5nIrnxQiTNgDGqfHM2JYQxbrV0',
+        expires: 1484314697598
+      }*/
+
+      /*service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'admin@tradersplanet.us',
+        clientId:
+          '481274889427-852dmbk1rbr32rktim7kcv65r2nri00t.apps.googleusercontent.com',
+        clientSecret: 'lmV9O0iBV0VsCE4v_BLUUJdo',
+        refreshToken:
+          '1/IwQviU9lUMaKxsDhDkZbSZezw1xciUClIiJt-RMjYsT8UazsjOCXqk-bW0ezxoqa',
+        accessToken: accessToken
+      }*/
     });
 
+    await transporter.verify()
+
     // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: `"${name} 👻" <${email}>`,
-      to: email,//"info@tradersplanet.us",
+    let info = await transport.sendMail({
+      from: `"Administrador" <admin@tradersplanet.us>`,
+      to: email, //"info@tradersplanet.us",
       subject,
       text
     });
 
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   } catch (error) {
-    console.log(error);
+    console.log(error.toString());
   }
 }
 
-sendMail('Informacion', 'HOla como estas??', 'Jose', 'blackencio33@gmail.com')
+sendMail('Informacion', 'HOla como estas??', 'Jose', 'blackencio33@gmail.com');
