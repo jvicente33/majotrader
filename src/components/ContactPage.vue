1da9fd6d58c0a663d7bb6b8715d72eede57b3108
J. Daza <blackencio33@gmail.com>
2019-05-21 21:57:21 -0400

    cambio nodemailer por sendgrid admin@tradersplanet.us

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -107,7 +107,7 @@ export default {
   methods: {
     async sendEmail() {
       axios
-        .post("http://localhost:8020/send/contact", {
+        .post("https://tradersplanet.us/send/contact", {
             nombre: this.datos.name,
             email: this.datos.email,
             asunto: this.datos.subject,
