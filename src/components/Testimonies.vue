6f70c3eecba3d46bd533aa6ac5f4d46dce8b36ab
J. Daza <blackencio33@gmail.com>
2019-05-29 09:41:23 -0400

    subiendo nuevos testimonios

--- src/components/Testimonies.vue
+++ src/components/Testimonies.vue
@@ -33,7 +33,7 @@
                 </div>-->
               </div>
               <p
-                class="text-white"
+                class="text-white" align="justify"
               >Si quieren tener libertad financiera recomiendo el curso de María José me encanto como explica la clase una persona muy humilde explica buenísimo el stock market sabe mucho del tema.</p>
             </div>
             <div class="single-feedback-carusel">
@@ -48,7 +48,7 @@
                 </div>-->
               </div>
               <p
-                class="text-white"
+                class="text-white" align="justify"
               >Hola, gracias por transmitir esas energías positivas cada mañana y que tanto me han ayudado a crecer en este mundo del trading. Eres lo máximo, y sobre todo tu humildad y sinceridad me han hecho creer en ti cada día más.</p>
             </div>
             <div class="single-feedback-carusel">
@@ -63,8 +63,38 @@
                 </div>-->
               </div>
               <p
-                class="text-white"
+                class="text-white" align="justify"
               >Maria jose eres la mejor guia o mentor que hemos podido tener, porque siempre estas allí, cualquier duda o pregunta siempre la respondes, incluso hasta tarde noche, gracias.</p>
+            </div>
+            <div class="single-feedback-carusel">
+              <div class="title d-flex flex-row">
+                <h4 class="text-white pb-10">Alvaro Vargas</h4>
+                <!-- <div class="star">
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </div>-->
+              </div>
+              <p
+                class="text-white" align="justify"
+              >Mary como estas te escribo para agradecerte por todo  y por tu gran conocimiento en este mundo del trading ,entre a tu curso sin saber  q era una vela japonesa y hoy en dia por tu forma tan simple y eficaz de explicar de enseñar y sobre todo tu apoyo y pasiencia hoy puedo decir soy un trader aunque apenas estoy en mis pininos estoy aplicando todos los pasos  y cada uno de los puntos importantes que aprendi en las clases y hasta ahora eh tenido buenos resultados ,de nuevo muchas gracias me has dado otra manera de crear un ingreso.</p>
+            </div>
+             <div class="single-feedback-carusel">
+              <div class="title d-flex flex-row">
+                <h4 class="text-white pb-10">Kevin Sanchez</h4>
+                <!-- <div class="star">
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </div>-->
+              </div>
+              <p
+                class="text-white" align="justify"
+              >Tenía muchas especulaciones y dudas antes de empezar el curso, sobre todo la información que existe en internet. Pensando si esto sería algo que pueda aprender por mi cuenta o es algo de fantasía que te venden un curso para ganar dinero rápido como he visto en otras cuentas de Instagram. Pero cuando llegue al perfil de Majo me precaté que es alguien real y de confianza, apenas le escribí sobré el curso me dio la tranquilidad que es algo profesional. Durante el curso hasta terminarlo solo fue ratificando la buena decisión que fue estudiar con ella. La califico 10/10 porque en todo dio su milla extra, te transmite la pasión que tiene y siempre esta 24/7 para ti en cualquier duda que tengas. Yo la recomiendo y volvería a tener un curso con ella, he quedado más que satisfecho con el dinero invertido y le agradezco por todas sus enseñanzas.</p>
             </div>
           </div>
         </div>
