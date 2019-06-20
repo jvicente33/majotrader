6c0f5d1c85d8b72b898f951de01c792e3b336e8a
J. Daza <blackencio33@gmail.com>
2019-06-19 21:20:53 -0400

    add port

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,8 +5,8 @@ const NProgress = require('nprogress')
 NProgress.configure({ showSpinner: false });
 NProgress.configure({ minimum: 0.3 });
 
-const url = 'http://localhost:3001';
-//const url = 'http://panel.tradersplanet.us';
+//const url = 'http://localhost:3001';
+const url = 'https://tradersplanet-panel.herokuapp.com';
 
 // create a new axios instance
 const instance = axios.create({
