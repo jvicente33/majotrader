eb13fd10ef90ad6c32ebf59a8f9bc8ed9952c181
J. Daza <blackencio33@gmail.com>
2020-01-17 13:30:46 -0400

    change

--- src/utils/api.curso.js
+++ src/utils/api.curso.js
@@ -5,7 +5,7 @@ const data = [
         convertirte en un comerciante de las Opciones Financieras. Esta capacitación es desde cero no
         necesitas tener conocimiento previo sobre el mercado bursátil.`,
         img: 'curso-opciones-financieras.jpg',
-        price: 59000,
+        price: 70000,
         offprice: 0,
         temary: 'curso-opciones-financieras.pdf'
     },
